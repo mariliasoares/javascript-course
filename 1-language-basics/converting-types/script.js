4e78af9d235b6158dbdcb530d506d47a33e8b8af
Marilia Soares <marisoares1202@gmail.com>
2020-01-29 21:22:43 -0400

    converting types

---
+++ 1-language-basics/converting-types/script.js
@@ -0,0 +1,17 @@
+// ************ BEST PRACTICES *************
+// whatever you get from a user, from a HTML input in JS code will ALWAYS be a string.
+// to convert to a number instead of string concatenation -> mathematical equations
+
+//take a string as an input and convert text to a number without decimal parts (parseFloat())
+//parseInt();
+
+//if a number without decimal places, can be used +userInput
+
+//to convert a number to a string
+//result.toString();
+
+//shortcuts
+// instead   currentResult = currentResult + enteredNumber;
+// do        currentResult += enteredNumber;
+
+// a semicolon is used after {} if the {} are on the right side of the equal sign!
