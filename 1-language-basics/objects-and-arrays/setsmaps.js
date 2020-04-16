c4ef5b5b9ac2d82b29a241080dd28b2d62007a45
Marilia <marisoares1202@gmail.com>
2020-04-15 21:54:56 -0300

    that is maps

--- 1-language-basics/objects-and-arrays/setsmaps.js
+++ 1-language-basics/objects-and-arrays/setsmaps.js
@@ -8,3 +8,27 @@ for (const entry of ids.entries()) { //returns the same value twice
   //console.log(entry);
   console.log(entry[0]);
 }
+
+// **** MAPS
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Marilia'};
+
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}])
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const entry of personData.entries()) {
+  console.log(entry);
+}
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
